69acb6c18e59d86638548078d4926d11ece7307a
SmartDev116 <williambennett116ok@proton.me>
2023-04-29 20:32:27 -0700

    generators: tar: Fixed how CONFIG options are parsed

--- generators/tar/package-tar.sh
+++ generators/tar/package-tar.sh
@@ -1,19 +1,18 @@
 #!/bin/bash -e
 
 ROOTFS="$WORK/rootfs/"
-OUTPUT="$WORK/output/"
+OUTPUT="$WORK/output"
 TARFILE="${OUTPUT}/rootfs.tar"
 
 mkdir -p "$OUTPUT"
 
-COMPRESSION=""
-
-if [[ "$IMAGE_ROOTFS_TAR_NONE" ]]; then
+if [[ "$IMAGE_ROOTFS_TAR_NONE" == 'y' ]]; then
         COMPRESSION=""
-elif [[ "$IMAGE_ROOTFS_TAR_XZ" ]]; then
+elif [[ "$IMAGE_ROOTFS_TAR_XZ" == 'y' ]]; then
         COMPRESSION="J"
+        export XZ_OPT="-T0" # use multiple threads
         TARFILE="${TARFILE}.xz"
-elif [[ "$IMAGE_ROOTFS_TAR_BZIP2" ]]; then
+elif [[ "$IMAGE_ROOTFS_TAR_BZIP2" == 'y' ]]; then
         COMPRESSION="j"
         TARFILE="${TARFILE}.bz2"
 else 
