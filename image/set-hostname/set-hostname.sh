a9d0836b3ac90a0164b3abf93f25aa42a806de9f
SmartDev116 <williambennett116ok@proton.me>
2023-05-01 16:42:39 -0700

    set-hostname: Set -e on script to detect failures

--- image/set-hostname/set-hostname.sh
+++ image/set-hostname/set-hostname.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
 
 HOSTNAME="$CONFIG_DS_HOSTNAME"
-
 if [[ -z "$HOSTNAME" ]]; then
     HOSTNAME="$DS_DISTRO-$DS_RELEASE-$DS_TARGET_ARCH"
 fi
-
 echo "$HOSTNAME" > /etc/hostname
 echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
