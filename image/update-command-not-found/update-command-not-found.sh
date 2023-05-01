8d20cb7d0232dcade9c40e0e4263485b9954fbd3
SmartDev116 <williambennett116ok@proton.me>
2023-05-01 16:55:58 -0700

    update-command-not-found: Fail if command-not-found is not installed

--- image/update-command-not-found/update-command-not-found.sh
+++ image/update-command-not-found/update-command-not-found.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
+
+if ! which update-command-not-found >/dev/null; then
+        echo "command-not-found is not installed"
+        exit 1
+fi
 
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 apt-get update
