636447ab9257bc6ed2584d75a3c400fe4844d01e
SmartDev116 <williambennett116ok@proton.me>
2023-05-01 11:38:33 -0700

    distros: build.sh: Added a human readable portion of the distro cache key

--- distros/build.sh
+++ distros/build.sh
@@ -41,6 +41,7 @@ EOF
 # For Debian based distributions we checksum the multistrap config
 # which comprises our arch/distro/release/sourceurl/
 DISTRO_CACHE_KEY=$(sha256sum $MULTISTRAPCONF | cut -f 1 -d ' ')
+DISTRO_CACHE_KEY="${DISTRO}-${RELEASE}-${TARGET_ARCH}-${DISTRO_CACHE_KEY}"
 
 if ! common/fetch_cache_obj.sh "$DISTRO_CACHE_KEY" "$INSTALL"; then
         /usr/sbin/multistrap -f "$MULTISTRAPCONF" -d "$INSTALL"
