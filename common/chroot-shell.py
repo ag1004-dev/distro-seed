2385461b11e05c125b02c73cf259a2491cf2e9f2
SmartDev116 <williambennett116ok@proton.me>
2023-05-01 15:48:18 -0700

    chroot-shell.py: Updated for refactored config options.
    chroot-shell now has access to all of the CONFIG_ options

--- common/chroot-shell.py
+++ common/chroot-shell.py
@@ -12,18 +12,38 @@ kconf = kconfiglib.Kconfig('Kconfig')
 kconf.load_config('.config')
 set_kconfig_vars(kconf)
 
-HOST_ROOT_PATH = os.environ['HOST_ROOT_PATH']
-WORK = os.environ['WORK']
-TAG = os.environ['TAG']
+HOST_ROOT_PATH = os.environ['DS_HOST_ROOT_PATH']
+WORK = os.environ['DS_WORK']
+TAG = os.environ['DS_TAG']
 dockerenv = os.path.abspath(WORK + "/dockerenv")
+
+# Get all kconfig values in to key=value string list
+config_dict={}
+for key, value in kconf.syms.items():
+    # Skip the preset MODULES symbol which does not apply here
+    if key == 'MODULES':
+        continue
+    config_dict[f'CONFIG_{key}'] = value.str_value
+
+command = [ 'docker', 'run', '-it',
+        '--volume', f'{HOST_ROOT_PATH}:/work/',
+        '--workdir', '/work/' ]
+
+if os.path.exists(dockerenv):
+    command.append('--env-file')
+    command.append(f'{dockerenv}')
+
+for key, value in config_dict.items():
+    command.append('-e')
+    command.append(f'{key}={value}')
+
+command.append(TAG)
+command.append('chroot')
+command.append('/work/work/rootfs')
+command.append('/bin/bash')
 
 result = subprocess.run(['which', 'docker'], capture_output=True,
                         text=True, check=True)
 docker_path = result.stdout.strip()
 
-os.execl(docker_path, 'docker', 'run', '-it',
-         '--volume', f'{HOST_ROOT_PATH}:/work/',
-         '--env-file', f'{dockerenv}',
-         '--workdir', '/work/',
-         TAG,
-         'chroot', '/work/work/rootfs', '/bin/bash' )
+os.execl(docker_path, *command )
