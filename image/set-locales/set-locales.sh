ea95acc67ba6987954ec4c6dcd0b46b1f3f4f1db
SmartDev116 <williambennett116ok@proton.me>
2023-05-01 17:03:38 -0700

    set-locales: Added -e to catch failures, but skip if locales are not installed

--- image/set-locales/set-locales.sh
+++ image/set-locales/set-locales.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
+
+if [ ! -e /etc/locale.gen ]; then
+    echo "locales package is not installed, skipping configuring locales"
+    # this is not considered an error because locales configuration is not optional
+    # if locales are installed.  Usually this should be included.
+    exit 0
+fi
 
 sed -i -e "s/# $CONFIG_DS_LOCALES.*/$CONFIG_DS_LOCALES UTF-8/" /etc/locale.gen
 dpkg-reconfigure --frontend=noninteractive locales
