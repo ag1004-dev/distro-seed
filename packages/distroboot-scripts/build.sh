4df88417d07ad8d1ffbcfdff5b8d077d35197b38
SmartDev116 <williambennett116ok@proton.me>
2023-05-03 09:01:22 -0700

    packages: Distroboot-scripts: changed WORK to new DS_WORK

--- packages/distroboot-scripts/build.sh
+++ packages/distroboot-scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INSTALL="$WORK/deploy/15-bootscripts/"
+INSTALL="$DS_WORK/deploy/15-bootscripts/"
 PACKAGE_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 mkdir -p "$INSTALL/boot/"
