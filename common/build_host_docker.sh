aea6301800a0b009ade5161b714cce27cf72524b
SmartDev116 <williambennett116ok@proton.me>
2023-05-03 08:35:27 -0700

    common: build_host_docker.sh: Dont specify quiet on initial docker build to help debug failures

--- common/build_host_docker.sh
+++ common/build_host_docker.sh
@@ -11,8 +11,9 @@ cd "$dockerpath"
 
 if [[ "$(docker images -q $DS_TAG 2> /dev/null)" == "" ]]; then
     echo "Generating docker, this can take a while on the first build"
+    docker build --tag "$DS_TAG" .
+else
+    # If the docker has already been built, this generally returns instantly.
+    # In case there were changes though, we rebuild.
+    docker build --quiet --tag "$DS_TAG" .
 fi
-
-# If the docker has already been built, this generally returns instantly.
-# In case there were changes though, we rebuild.
-docker build --quiet --tag "$DS_TAG" .
