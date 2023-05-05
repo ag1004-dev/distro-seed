0636084368cfb9d6af790fa8e64fa1f277a22c7e
SmartDev116 <williambennett116ok@proton.me>
2023-05-05 16:50:47 -0700

    kernel: gitkernel: fetch: Caching key cleaned up to include defconfig

--- kernel/gitkernel/fetch.sh
+++ kernel/gitkernel/fetch.sh
@@ -1,16 +1,12 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
-SOURCE="${DS_WORK}/kernel/linux/"
-KERNEL_CACHE_KEY="${DS_WORK}/kernel/linux-cache-key"
-GITURL="$CONFIG_DS_KERNEL_PROVIDER_GIT_URL"
-GITVERSION="$CONFIG_DS_KERNEL_PROVIDER_GIT_VERSION"
+SOURCE="$DS_WORK/kernel/linux/"
+KERNEL_CACHE_KEY="$DS_WORK/kernel/linux-cache-key"
 
 # Create caching key
 mkdir -p "$DS_WORK/kernel"
-# The key must include anything unique about this build
-CACHE_KEY="$(common/host/gen_cache_key.sh $GITURL $GITVERSION $KERNEL_DEFCONFIG)"
+CACHE_KEY=$(common/host/gen_cache_key.sh "$CONFIG_DS_KERNEL_PROVIDER_GIT_URL $CONFIG_DS_KERNEL_PROVIDER_GIT_VERSION $CONFIG_DS_KERNEL_DEFCONFIG")
 echo "$CACHE_KEY" > "$KERNEL_CACHE_KEY"
 
 mkdir -p "$SOURCE"
-
-common/host/fetch_git.sh "$GITURL" "$GITVERSION" "$SOURCE"
+common/host/fetch_git.sh "$CONFIG_DS_KERNEL_PROVIDER_GIT_URL" "$CONFIG_DS_KERNEL_PROVIDER_GIT_VERSION" "$SOURCE"
