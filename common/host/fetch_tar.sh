78bd988af8129d13d3fe518a8b74875cb3115990
SmartDev116 <williambennett116ok@proton.me>
2023-05-09 22:05:49 -0700

    common: host: fetch_tar: Initial commit of tool to fetch and cache tar files

---
+++ common/host/fetch_tar.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -ex
+
+url="$1"
+work_path="$2"
+
+file_name=$(basename "$url")
+
+file_path="$DS_DL/tar/$file_name"
+mkdir -p "$DS_DL/tar/"
+
+if [ -f "$file_path" ] && [ -f "${file_path}.sha256" ]; then
+	set +e
+	if sha256sum --quiet -c "$file_path.sha256"; then
+		set -e
+		tar -xf "$file_path" -C "$work_path"
+		exit 0
+	fi
+fi
+
+set -e
+
+wget -O "$file_path" "$url"
+sha256sum "$file_path" > "$file_path.sha256"
+tar -xf "$file_path" -C "$work_path"
