17f0838dbf3387a8370e1c0efe15be960032e86a
SmartDev116 <williambennett116ok@proton.me>
2023-05-11 13:01:27 -0700

    update-command-not-found: Remove manual set up of resolv.conf
    This is now set up by apt_finish_install.sh

--- image/update-command-not-found/update-command-not-found.sh
+++ image/update-command-not-found/update-command-not-found.sh
@@ -5,7 +5,5 @@ if ! which update-command-not-found >/dev/null; then
         exit 1
 fi
 
-echo "nameserver 1.1.1.1" > /etc/resolv.conf
 apt-get update
 update-command-not-found
-rm /etc/resolv.conf
