8cb6a2e47ce7d038d83d6a2ee972598e92810a3f
SmartDev116 <williambennett116ok@proton.me>
2023-05-24 11:02:27 -0700

    PACKAGE -> COMPONENT in build/install

--- tasks/components/ts-uboot-env-configs/install.sh
+++ tasks/components/ts-uboot-env-configs/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-PACKAGE_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+COMPONENT_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 mkdir -p "$DS_OVERLAY/usr/share/uboot-env-configs/"
-cp "$PACKAGE_PATH"/files/* "$DS_OVERLAY/usr/share/uboot-env-configs/"
+cp "$COMPONENT_PATH"/files/* "$DS_OVERLAY/usr/share/uboot-env-configs/"
