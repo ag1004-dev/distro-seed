46b29c329c59e983f73d36b3d8558206e12cce76
SmartDev116 <williambennett116ok@proton.me>
2023-06-13 16:20:03 -0700

    distro-seed:#3:tasks/components/resize/install.sh:implemented install -m 755

--- tasks/components/resize/install.sh
+++ tasks/components/resize/install.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 mkdir -p "$DS_OVERLAY/usr/local/bin/"
-cp "$SCRIPT_PATH"/files/resize "$DS_OVERLAY/usr/local/bin/resize"
+install -m 755 "$SCRIPT_PATH"/files/resize "$DS_OVERLAY/usr/local/bin/resize"
