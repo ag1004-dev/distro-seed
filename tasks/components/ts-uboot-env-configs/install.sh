6bc9fd636dd3b0cd7e2954a6db4e5e2706f78359
SmartDev116 <williambennett116ok@proton.me>
2023-06-13 18:02:14 -0700

    distro-seed: #3 : tasks/components/wilc-firmware/fetch.sh: implemented install -m 644

--- tasks/components/ts-uboot-env-configs/install.sh
+++ tasks/components/ts-uboot-env-configs/install.sh
@@ -2,4 +2,4 @@
 
 COMPONENT_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 mkdir -p "$DS_OVERLAY/usr/share/uboot-env-configs/"
-cp "$COMPONENT_PATH"/files/* "$DS_OVERLAY/usr/share/uboot-env-configs/"
+install -m 644 "$COMPONENT_PATH"/files/* "$DS_OVERLAY/usr/share/uboot-env-configs/"
