f0f7172eb8dedd01b55f23a3fde9d22a28460040
SmartDev116 <williambennett116ok@proton.me>
2023-06-26 15:39:23 -0700

    ts-uboot-env-configs: install.sh: Fixed typo

--- tasks/components/ts-uboot-env-configs/install.sh
+++ tasks/components/ts-uboot-env-configs/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 COMPONENT_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-isntall -d "$DS_OVERLAY/usr/share/uboot-env-configs/"
+install -d "$DS_OVERLAY/usr/share/uboot-env-configs/"
 install -m 644 "$COMPONENT_PATH"/files/* "$DS_OVERLAY/usr/share/uboot-env-configs/"
