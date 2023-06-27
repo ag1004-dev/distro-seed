e1139bdffb0bfd2d14802afc3eaf89215359b89f
SmartDev116 <williambennett116ok@proton.me>
2023-06-27 15:04:55 -0700

    tasks: chroot_prep: Allow skipping dev node creation
    This is useful if the build fails for any reason, this lets 'make chroot-shell' still enter the
    chroot environment without it having been cleaned up.

--- tasks/core/chroot_prep/chroot_prep.sh
+++ tasks/core/chroot_prep/chroot_prep.sh
@@ -6,8 +6,12 @@
 QEMU_STATIC_PATH=$(which ${DS_QEMU_STATIC})
 cp "${QEMU_STATIC_PATH}" "${DS_WORK}/rootfs/${QEMU_STATIC_PATH}"
 
-mknod -m 666 "${DS_WORK}/rootfs/dev/zero" c 1 5
-mknod -m 666 "${DS_WORK}/rootfs/dev/null" c 1 3
+if [ ! -e "${DS_WORK}/rootfs/dev/zero" ]; then
+    mknod -m 666 "${DS_WORK}/rootfs/dev/zero" c 1 5
+fi
+if [ ! -e "${DS_WORK}/rootfs/dev/null" ]; then
+    mknod -m 666 "${DS_WORK}/rootfs/dev/null" c 1 3
+fi
 
 # Set up a temporary resolv.conf.
 if [ -L "/etc/resolv.conf" ]; then
