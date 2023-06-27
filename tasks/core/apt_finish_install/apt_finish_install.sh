6ec20a2e1393e58cf15d69b710c97efbe249ab38
SmartDev116 <williambennett116ok@proton.me>
2023-06-27 15:03:23 -0700

    tasks: apt_finish_install: Added ubuntu lunar workaround.  Removed old passwd call which is now in set-users

--- tasks/core/apt_finish_install/apt_finish_install.sh
+++ tasks/core/apt_finish_install/apt_finish_install.sh
@@ -3,9 +3,16 @@
 export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
 export LC_ALL=C LANGUAGE=C LANG=C
 dpkg --configure -a
+
+if [ "$DS_DISTRO" == "ubuntu" ] && [ "$DS_RELEASE" == "lunar" ]; then
+    # Workaround for:
+    # Setting up sgml-base (1.31) ...
+    # cannot open catalog directory /etc/sgml: No such file or directory at /usr/sbin/update-catalog line 299.
+    mkdir /etc/sgml/
+fi
+
 apt-get install -f
 apt-get clean
-passwd --delete root
 chmod 755 /
 
 # Set up a temporary resolv.conf.
