bcf678b2d578794fd6b75c89e25f31e69046e38a
SmartDev116 <williambennett116ok@proton.me>
2024-01-26 16:11:04 -0700

    Honor CONFIG_DS_COMPONENT_DISTROBOOT_SCRIPTS_INSTALL_SOURCE

--- tasks/components/distroboot-scripts/build.sh
+++ tasks/components/distroboot-scripts/build.sh
@@ -3,3 +3,7 @@
 install -d "$DS_OVERLAY/boot/"
 mkimage -A arm -T script -C none -n 'boot' \
         -d "${DS_TASK_PATH}/files/boot.source" "${DS_OVERLAY}/boot/boot.scr"
+
+if [[ "${CONFIG_DS_COMPONENT_DISTROBOOT_SCRIPTS_INSTALL_SOURCE}" == 'y' ]]; then
+    install --target-directory="$DS_OVERLAY/boot/" "${DS_TASK_PATH}/files/boot.source"
+fi
