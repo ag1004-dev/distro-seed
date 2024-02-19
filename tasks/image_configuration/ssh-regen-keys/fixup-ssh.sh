8858bb83ea55b2090b75daeb72be7f7fe88fa590
SmartDev116 <williambennett116ok@proton.me>
2024-02-19 22:20:56 +0000

    image/ssh-regen: remove keys and always regen if missing
    Enabling this config options will now remove all host keys generated
    at install time by APT when configuring sshd. Additionally, it will
    install a script to the target that will use ssh-keygen -A which will
    generate host keys if they are not present at boot.
    
    Fixes #41
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- tasks/image_configuration/ssh-regen-keys/fixup-ssh.sh
+++ tasks/image_configuration/ssh-regen-keys/fixup-ssh.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 if [ ! -e "/usr/sbin/sshd" ]; then
-    echo "ssh not installed"
+    echo -e "\n======================================================================="
+    echo -e "\tsshd not installed in target!"
+    echo -e "\tEither use a packagelist that includes sshd or disable"
+    echo -e "\tDS_REGENERATE_SSH_KEYS in distro-seed config"
+    echo -e "=======================================================================\n"
     exit 1
 fi
 
-servicename=sshfirstboot.service
+servicename=sshkeys.service
 servicefile="/etc/systemd/system/${servicename}"
 runscript="/usr/local/bin/regen_ssh_keys"
 
-touch /ssh_regenkeys
+rm -f /etc/ssh/ssh_host_*
 
 cat <<EOF > "$servicefile"
 [Unit]
-Description=Regenerate SSH keys for first boot
-ConditionPathExists=/ssh_regenkeys
+Description=Regenerate SSH keys if they do not exist
 Before=ssh.service
 
 [Service]
@@ -32,7 +35,6 @@ if [ -e "/usr/sbin/sshd" ]; then
     ssh-keygen -A
 fi
 
-rm /ssh_regenkeys
 EOF
 
 chmod a+x "$runscript"
